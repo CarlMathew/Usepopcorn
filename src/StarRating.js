ee8c354705fc4126903b6f552a13a232f03b95b4
carlito <moradacarl2711@gmail.com>
2025-03-21 12:11:38 +0800

    fix new bug for the production

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 StarRating.propTypes = {
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Star from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <Star maximum="10" star_size={40} /> */}
   </React.StrictMode>
 );
